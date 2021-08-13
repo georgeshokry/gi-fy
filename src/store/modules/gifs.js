5e454933ade972918069e1c1e551800f0218f415
georgeshokry <georgeshokry5@gmail.com>
2021-08-13 16:30:35 +0200

    vuetify & store

---
+++ src/store/modules/gifs.js
@@ -0,0 +1,24 @@
+// import axios from 'axios';
+
+const state = {
+  gifs: []
+};
+
+const getters = {
+
+};
+
+const actions = {
+
+};
+
+const mutations = {
+
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
